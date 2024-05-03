5cb342332620b538602ed6bff9b03001139415c6
Halil Cosdu <halilcosdu@gmail.com>
2024-05-03 18:51:57 +0300

    Tests added.

--- tests/SlowerTest.php
+++ tests/SlowerTest.php
@@ -1,1 +1,21 @@
 <?php
+
+use HalilCosdu\Slower\Services\RecommendationService;
+use HalilCosdu\Slower\Slower;
+
+beforeEach(function () {
+    $this->mockedRecommendationService = Mockery::mock(RecommendationService::class);
+    $this->slower = new Slower($this->mockedRecommendationService);
+});
+
+it('analyzes a model', function () {
+    $mockedModel = Mockery::mock(config('slower.resources.model'));
+
+    $expectedResult = $mockedModel;
+
+    $this->mockedRecommendationService->shouldReceive('getRecommendation')->once()->with($mockedModel);
+
+    $result = $this->slower->analyze($mockedModel);
+
+    expect($result)->toBe($expectedResult);
+});
