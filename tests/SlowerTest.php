f5a7516533702061d31e1dbe2ae36d528e4f9e9d
Halil Cosdu <halilcosdu@gmail.com>
2024-05-04 17:23:30 +0300

    tests updated.

--- tests/SlowerTest.php
+++ tests/SlowerTest.php
@@ -7,15 +7,43 @@ beforeEach(function () {
     $this->mockedRecommendationService = Mockery::mock(RecommendationService::class);
     $this->slower = new Slower($this->mockedRecommendationService);
 });
+describe('analyze', function () {
+    it('throws an exception if the model is not an instance of the configured model', function () {
+        class TestModel extends Illuminate\Database\Eloquent\Model
+        {
+        }
+        $mockedModel = Mockery::mock(TestModel::class);
+        $this->expectException(InvalidArgumentException::class);
+        $this->expectExceptionMessage('Model must be an instance of '.config('slower.resources.model'));
+        $this->slower->analyze($mockedModel);
+    });
 
-it('analyzes a model', function () {
-    $mockedModel = Mockery::mock(config('slower.resources.model'));
+    it('analyzes a model', function () {
+        $mockedModel = Mockery::mock(config('slower.resources.model'));
 
-    $expectedResult = $mockedModel;
+        $expectedResult = $mockedModel;
 
-    $this->mockedRecommendationService->shouldReceive('getRecommendation')->once()->with($mockedModel);
+        $this->mockedRecommendationService->shouldReceive('getRecommendation')->once()->with($mockedModel);
 
-    $result = $this->slower->analyze($mockedModel);
+        $result = $this->slower->analyze($mockedModel);
 
-    expect($result)->toBe($expectedResult);
+        expect($result)->toBe($expectedResult);
+    });
+
+    it('does not call the recommendation service if AI recommendation is disabled', function () {
+        config(['slower.ai_recommendation' => false]);
+        $mockedModel = Mockery::mock(config('slower.resources.model'));
+        $this->mockedRecommendationService->shouldReceive('getRecommendation')->never();
+        $result = $this->slower->analyze($mockedModel);
+        expect($result)->toBe($mockedModel);
+    });
+
+    it('returns the analyzed model', function () {
+        config(['slower.ai_recommendation' => true]);
+        $mockedModel = Mockery::mock(config('slower.resources.model'));
+        $expectedResult = $mockedModel;
+        $this->mockedRecommendationService->shouldReceive('getRecommendation')->once()->with($mockedModel);
+        $result = $this->slower->analyze($mockedModel);
+        expect($result)->toBe($expectedResult);
+    });
 });
