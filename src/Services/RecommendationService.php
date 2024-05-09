327a29175b426378f74cd0861d2a31d81a620d9e
Halil Cosdu <halilcosdu@gmail.com>
2024-05-09 23:22:00 +0300

    Current indexes and schema added.

--- src/Services/RecommendationService.php
+++ src/Services/RecommendationService.php
@@ -2,6 +2,7 @@
 
 namespace HalilCosdu\Slower\Services;
 
+use Illuminate\Support\Facades\DB;
 use OpenAI\Client;
 
 class RecommendationService
@@ -14,14 +15,18 @@ class RecommendationService
     public function getRecommendation($record): ?string
     {
 
+        [$indexes, $schema] = $this->extractIndexesAndSchemaFromRecord($record);
+
         $result = $this->client->chat()->create([
             'model' => config('slower.recommendation_model', 'gpt-4'),
             'messages' => [
                 ['role' => 'system', 'content' => config('slower.prompt')],
                 ['role' => 'user', 'content' => 'The query execution took '.$record->time.' milliseconds.'.PHP_EOL.
-                                    'Connection: '.$record->connection.PHP_EOL.
-                                    'Connection Name: '.$record->connection.PHP_EOL.
-                                    'Sql: '.$record->raw_sql,
+                    'Connection: '.$record->connection.PHP_EOL.
+                    'Current Indexes: '.json_encode($indexes, JSON_PRETTY_PRINT).PHP_EOL.
+                    'Schema: '.json_encode($schema, JSON_PRETTY_PRINT).PHP_EOL.
+                    'Connection Name: '.$record->connection_name.PHP_EOL.
+                    'Sql: '.$record->raw_sql,
                 ],
             ],
         ]);
@@ -34,5 +39,22 @@ class RecommendationService
         );
 
         return $result->choices[0]->message->content;
+    }
+
+    private function extractIndexesAndSchemaFromRecord($record): array
+    {
+        $schemaBuilder = DB::connection($record->connection)->getSchemaBuilder();
+
+        $columns = $schemaBuilder->getColumnListing($record->getTable());
+
+        $indexes = $schemaBuilder->getIndexes($record->getTable());
+
+        $schema = [];
+
+        foreach ($columns as $column) {
+            $schema[$column] = $schemaBuilder->getColumnType($record->getTable(), $column);
+        }
+
+        return [$indexes, $schema];
     }
 }
