76199a0ce659c93c4ce98f2951745c53f46e5f66
halilcosdu <halilcosdu@users.noreply.github.com>
2024-08-10 11:10:53 +0000

    Fix styling

--- tests/SlowerTest.php
+++ tests/SlowerTest.php
@@ -9,9 +9,7 @@ beforeEach(function () {
 });
 describe('analyze', function () {
     it('throws an exception if the model is not an instance of the configured model', function () {
-        class TestModel extends Illuminate\Database\Eloquent\Model
-        {
-        }
+        class TestModel extends Illuminate\Database\Eloquent\Model {}
         $mockedModel = Mockery::mock(TestModel::class);
         $this->expectException(InvalidArgumentException::class);
         $this->expectExceptionMessage('Model must be an instance of '.config('slower.resources.model'));
